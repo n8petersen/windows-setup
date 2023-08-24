ba1d9f687b5879a3788db6078b45e02d30dd5694
Nathan Petersen <n8knows@gmail.com>
2023-08-24 15:31:38 -0600

    Update ApplicationInstall.ps1
    Fixed Choco initial install

--- ApplicationInstall.ps1
+++ ApplicationInstall.ps1
@@ -12,7 +12,7 @@ $testchoco = powershell choco -v
 if (-not($testchoco)) {
     Write-Output "Seems Chocolatey is not installed, installing now."
     Write-Host "----------------------------------------------------"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 else {
     Write-Output "Chocolatey Version $testchoco is already installed, skipping Chocolatey Install."
